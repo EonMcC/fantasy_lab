081422f524d3da18e14cd36ff77bbf8ba48446bb
Iain <iain2eyes@gmail.com>
2020-01-17 12:34:32 +0000

    Add Alice class

---
+++ src/main/java/characters/Alice.java
@@ -0,0 +1,12 @@
+package characters;
+
+import components.WeaponType;
+
+public class Alice extends Fighter {
+
+    public Alice(String name, int maxHP, String catchPhrase, WeaponType weapon) {
+        super(name, maxHP, catchPhrase, weapon);
+    }
+
+
+}
